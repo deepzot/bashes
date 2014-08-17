85b343ca7a46faf791224f8e23b88b1e4040b6a1
David Kirkby <dkirkby@uci.edu>
2014-08-17 12:15:24 -0700

    Create great3.Observation to manage access to GREAT3 files

--- bashes/great3.py
+++ bashes/great3.py
@@ -1,6 +1,93 @@
+import os
+import os.path
 import numpy as np
-
+from astropy.io import fits
+import yaml
 import galsim
+
+class Observation(object):
+    """
+    Represents a GREAT3 observation specified by a branch, field and epoch.
+    """
+    def __init__(self,path,field,epoch):
+        """
+        Initializes a branch using a path of the form 'control/ground/constant'
+        that should be present under $GREAT3_ROOT (and also under $GREAT3_ROOT/truth
+        if truth info is required). Raises a RuntimeError if any problems are detected.
+        """
+        if 'GREAT3_ROOT' not in os.environ:
+            raise RuntimeError('$GREAT3_ROOT is not set.')
+        g3root = os.environ['GREAT3_ROOT']
+        self.branchPath = os.path.join(g3root,path)
+        if not os.path.isdir(self.branchPath):
+            raise RuntimeError('No such branch path: %r' % self.branchPath)
+        # Do we have truth info available?
+        self.truthPath = os.path.join(g3root,'truth',path)
+        if not os.path.isdir(self.truthPath):
+            self.truthPath = None
+        # Check the field and epoch parameters.
+        try:
+            self.field = int(field)
+            assert self.field >= 0 and self.field < 200
+        except (ValueError,AssertionError):
+            raise RuntimeError('Invalid branch field index: %r' % field)
+        try:
+            self.epoch = int(epoch)
+            assert self.epoch >= 0
+        except (ValueError,AssertionError):
+            raise RuntimeError('Invalid branch epoch index: %r' % epoch)
+
+    def getImage(self):
+        """
+        Returns the array of postage stamp image data for this observation and initializes
+        our stampSize data member.
+        """
+        dataStampsPath = os.path.join(self.branchPath,'image-%03d-%d.fits' % (
+            self.field,self.epoch))
+        hduList = fits.open(dataStampsPath)
+        dataStamps = hduList[0].data
+        hduList.close()
+        # Infer the stamp size from the stamp array shape.
+        assert dataStamps.shape[0] == dataStamps.shape[1], 'Image data is not square'
+        assert dataStamps.shape[0] % 100 == 0, 'Image data does not consist of 100x100 stamps'
+        self.stampSize = dataStamps.shape[0]//100
+        return dataStamps
+
+    def getStars(self):
+        """
+        Returns the array of postage stamp starfield data for this observation.
+        """
+        psfStampsPath = os.path.join(self.branchPath,'starfield_image-%03d-%d.fits' % (
+            self.field,self.epoch))
+        hduList = fits.open(psfStampsPath)
+        psfStamps = hduList[0].data
+        hduList.close()
+        return psfStamps
+
+    def getTruthParams(self):
+        """
+        Returns a dictionary of truth parameter values for this observation.
+        """
+        if not self.truthPath:
+            raise RuntimeError('No truth available for observation')
+        truthParamsPath = os.path.join(self.truthPath,'epoch_parameters-%03d-%d.yaml' % (
+            self.field,self.epoch))
+        with open(truthParamsPath,'r') as f:
+            params = yaml.load(f)
+        return params
+
+    def getTruthCatalog(self):
+        """
+        Returns the truth catalog for this observation.
+        """       
+        if not self.truthPath:
+            raise RuntimeError('No truth available for observation')
+        truthCatalogPath = os.path.join(self.truthPath,'epoch_catalog-%03d-%d.fits' % (
+            self.field,self.epoch))
+        hduList = fits.open(truthCatalogPath)
+        truthCatalog = hduList[1].data
+        hduList.close()
+        return truthCatalog
 
 def createSource(params):
     """
