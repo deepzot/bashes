1a93822264cb16f4d6d014861e4f64234404c7e2
David Kirkby <dkirkby@uci.edu>
2014-08-25 17:59:30 -0700

    Add --nstamps option to pass multiple GREAT3 stamps to estimator

--- examples/g3bashes.py
+++ examples/g3bashes.py
@@ -14,6 +14,8 @@ def main():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     bashes.great3.Observation.addArgs(parser)
     bashes.Estimator.addArgs(parser)
+    parser.add_argument('--nstamps', type = int, default = 0,
+        help = 'number of stamps to include in the analysis (or 0 to use all stamps)')
     parser.add_argument('--save', type = str, default = 'bashes.npy',
         help = 'filename where analysis results will be saved in numpy format')
     args = parser.parse_args()
@@ -22,7 +24,11 @@ def main():
     obs = bashes.great3.Observation(**bashes.great3.Observation.fromArgs(args))
 
     # Load the postage stamps to analyze.
-    dataStamps = obs.getImage()
+    image = obs.getImage()
+    nstamps = args.nstamps if args.nstamps > 0 else image.nx*image.ny
+    dataStamps = np.empty((nstamps,obs.stampSize,obs.stampSize))
+    for i in range(nstamps):
+        dataStamps[i] = image.getStamp(i).array
 
     # Load the constant PSF stamp to use for the analysis.
     psfStamps = obs.getStars()
@@ -34,7 +40,7 @@ def main():
     # Build the estimator for this analysis (using only the first stamp, for now)
     psfModel = obs.createPSF(0)
     estimator = bashes.Estimator(
-        data=dataStamps.getStamp(0,0),psfs=psfModel,ivar=1./noiseVarTruth,
+        data=dataStamps,psfs=psfModel,ivar=1./noiseVarTruth,
         stampSize=obs.stampSize,pixelScale=obs.pixelScale,**bashes.Estimator.fromArgs(args))
 
     # Analyze stamps using the truth source prior for the first stamp.
