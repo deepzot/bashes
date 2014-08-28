01d498590193c95bd5e40de786f129d8191cc904
David Kirkby <dkirkby@uci.edu>
2014-08-28 15:51:06 -0700

    Plot a stamp/prior grid of NLL(g1,g2) contours

--- examples/g3post.py
+++ examples/g3post.py
@@ -15,6 +15,8 @@ def main():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--load', type = str, default = 'bashes',
         help = 'base filename of g3bashes job we will post process')
+    parser.add_argument('--grid', type = int, default = 0,
+        help = 'number of stamp/prior combinations to show as a grid of plots')
     parser.add_argument('--verbose', action = 'store_true',
         help = 'be verbose about progress')
     args = parser.parse_args()
@@ -26,19 +28,18 @@ def main():
     # Reconstruct the shear grid used by the estimator.
     ng = config['args']['ng']
     gmax = config['args']['gmax']
-    g1_center = config['args']['g1_center']
-    g2_center = config['args']['g2_center']
     dg = np.linspace(-gmax,+gmax,ng)
-    g1 = g1_center+dg
-    g2 = g2_center+dg
+    g1 = config['args']['g1_center'] + dg
+    g2 = config['args']['g2_center'] + dg
 
     # Prepare the shear grid edges needed by pcolormesh.
     g1e,g2e = bashes.utility.getBinEdges(g1,g2)
 
     # Initialize matplotlib.
-    fig = plt.figure('fig1',figsize=(12,9))
-    fig.set_facecolor('white')
-    #plt.subplots_adjust(left=0.05,bottom=0.06,right=0.98,top=0.98,wspace=0.1,hspace=0.1)
+    if args.grid:
+        gridFig = plt.figure('fig1',figsize=(12,12))
+        gridFig.set_facecolor('white')
+        plt.subplots_adjust(left=0.02,bottom=0.02,right=0.98,top=0.98,wspace=0.05,hspace=0.05)
 
     # Loop over priors.
     nstamps = config['args']['nstamps']
@@ -50,11 +51,21 @@ def main():
             continue
         nll = np.load(loadName)
         nllMin = np.min(nll)
-        ndata = nll.shape[0]
-        for idata in range(nstamps):
-            print 'prior %d, stamp %d' % (iprior,idata)
-            plt.pcolormesh(g1e,g2e,nll[:,idata].reshape((ng,ng))-nllMin,cmap='rainbow')
-            plt.show()
+        nllLevels = bashes.utility.getDeltaChiSq()
+        assert nll.shape[0] == ng*ng
+        ndata = nll.shape[1]
+        for idata in range(ndata):
+            nllShear = nll[:,idata].reshape((ng,ng))-nllMin
+            print 'prior %d, stamp %d, nllMin = %f (%f)' % (iprior,idata,np.min(nllShear),nllMin)
+            if args.grid and iprior < args.grid and idata < args.grid:
+                plt.subplot(args.grid,args.grid,iprior*args.grid+idata+1)
+                plt.pcolormesh(g1e,g2e,nllShear,cmap='rainbow')
+                plt.contour(g1,g2,nllShear,levels=nllLevels,colors='w',linestyles=('-','--',':'))
+                # Remove tick labels.
+                axes = plt.gca()
+                axes.xaxis.set_ticklabels([])
+                axes.yaxis.set_ticklabels([])
+    plt.show()
 
 if __name__ == '__main__':
     main()
