3e21ac9fdfa7cb9298a66c73b2583b0bdf002aa9
Daniel Margala <dmargala@uci.edu>
2014-10-08 15:52:29 -0700

    add astropy.io to list

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@ import os
 
 import mock
  
-MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate', 'galsim', 'scipy.special','astropy']
+MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate', 'galsim', 'scipy.special','astropy.io']
 for mod_name in MOCK_MODULES:
   sys.modules[mod_name] = mock.Mock()
 
